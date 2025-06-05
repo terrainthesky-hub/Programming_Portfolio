f08e01334c83c93cba3f76bef0a116c8a432222c
terrainthesky-hub <lesley.t.rich@gmail.com>
2025-06-05 16:42:05 -0700

    dependencies

---
+++ next.config.js
@@ -0,0 +1,32 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+// IMPORTANT: Replace 'your-repo-name' with the actual name of your repository
+if (isGithubActions) {
+  const repo = 'your-repo-name'; 
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  // Add this line to enable static exports
+  output: 'export',
+
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  
+  // Optional: If you are using Next.js 14+ and have images in your public/img folder,
+  // you might need to add this to disable image optimization which is not supported in static exports.
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
