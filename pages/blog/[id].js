a1cd31d8a5985fe5a7845179b3201c6837b7ff54
Lesley Rich <60892621+terrainthesky-hub@users.noreply.github.com>
2025-06-12 08:59:46 -0700

    Add contact section and white blog text

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -27,11 +27,11 @@ export default function Post({ postData }) {
       </Head>
       <article className="max-w-3xl mx-auto py-12">
         <div className="block bg-slate-800 rounded-lg shadow-lg hover:shadow-cyan-500/10 border border-slate-700 hover:border-teal-400 transition-all duration-300 p-6 cursor-pointer">
-          <h1 className="text-4xl font-extrabold text-gray-900 mb-2">{postData.title}</h1>
-          <p className="text-gray-500 mb-8">{postData.date}</p>
-          <div 
-            className="prose lg:prose-xl max-w-none" 
-            dangerouslySetInnerHTML={{ __html: postData.contentHtml }} 
+          <h1 className="text-4xl font-extrabold text-white mb-2">{postData.title}</h1>
+          <p className="text-gray-400 mb-8">{postData.date}</p>
+          <div
+            className="prose lg:prose-xl max-w-none text-white"
+            dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
           />
         </div>
       </article>
